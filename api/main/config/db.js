40731ab90302bcb3ec748c7b6544652d7aade84c
Sam Grund <sgrund626@gmail.com>
2020-09-17 18:03:06 -0400

    nuxt integration (#4)
    * think I have the nuxt integration working
    
    * nuxt ignore
    
    * remove build files
    
    * fix gitignore
    
    * config stuff

--- api/main/config/db.js
+++ api/main/config/db.js
@@ -1,24 +1,28 @@
 'use strict'
 
-const Sequelize = require('sequelize');
-const env = require('./env');
-const sequelize = new Sequelize(env.DATABASE, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
-  dialect: env.DATABASE_DIALECT,
-  storage: env.DATABASE_STORAGE,
-  define: {
-    underscored: true
+const Sequelize = require('sequelize')
+const env = require('./env')
+const sequelize = new Sequelize(
+  env.DATABASE,
+  env.DATABASE_USERNAME,
+  env.DATABASE_PASSWORD,
+  {
+    dialect: env.DATABASE_DIALECT,
+    storage: env.DATABASE_STORAGE,
+    define: {
+      underscored: true,
+    },
   }
-});
+)
 
-const db = {};
+const db = {}
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+db.Sequelize = Sequelize
+db.sequelize = sequelize
 
-db.patient = require('../models/Patient.js')(sequelize, Sequelize);
-db.sourceFile = require('../models/SourceFile.js')(sequelize, Sequelize);
+db.patient = require('../models/Patient.js')(sequelize, Sequelize)
+db.sourceFile = require('../models/SourceFile.js')(sequelize, Sequelize)
 
 db.sourceFile.hasMany(db.patient)
 
-
-module.exports = db;
+module.exports = db
