6393624a2f3b41a1e52a57468585dd872d1cf142
John (Jack) Ferguson <john.everett.ferguson@gmail.com>
2020-11-30 16:59:39 -0500

    feat: open the app (#90)

--- src/api/main/config/env.js
+++ src/api/main/config/env.js
@@ -4,12 +4,12 @@ const path = require('path')
 const env = {
   DATABASE_USERNAME: process.env.DATABASE_USERNAME || null,
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || null,
-  DATABASE: process.env.DATABASE || 'data.sqlite',
+  DATABASE: process.env.DATABASE || '',
   DATABASE_DIALECT: process.env.DATABASE_DIALECT || 'sqlite',
-  DATABASE_STORAGE: process.env.DATABASE_STORAGE || 'data.sqlite',
-  PORT: 8019,
   NODE_ENV: process.env.NODE_ENV || 'prod',
   BASE_URL: process.env.BASE_URL || 'http://localhost:8019',
+  PORT: 8019,
+  NODE_ENV: process.env.NODE_ENV || 'development',
 }
 
 module.exports = env
