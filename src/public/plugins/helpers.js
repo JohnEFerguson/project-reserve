5fc705569f1763ec4d1448d7080853a9645d4b87
Sam Grund <samuel.grund@1stdibs.com>
2021-01-09 13:55:07 -0500

    feat: only try one download case for the csv

--- src/public/plugins/helpers.js
+++ src/public/plugins/helpers.js
@@ -10,29 +10,38 @@ export function downloadCSV({ content, fileName }) {
   const a = document.createElement('a')
   const mimeType = 'text/csv;encoding:utf-8'
 
-  if (navigator.msSaveBlob) {
-    // IE10
-    navigator.msSaveBlob(
-      new Blob([content], {
-        type: mimeType,
-      }),
-      fileName
-    )
-  } else if (URL && 'download' in a) {
-    // html5 A[download]
-    a.href = URL.createObjectURL(
-      new Blob([content], {
-        type: mimeType,
-      })
-    )
-    a.setAttribute('download', fileName)
-    document.body.appendChild(a)
-    a.click()
-    document.body.removeChild(a)
-  } else {
-    location.href =
-      'data:application/octet-stream,' + encodeURIComponent(content) // only this mime type is supported
-  }
+  a.href = URL.createObjectURL(
+    new Blob([content], {
+      type: mimeType,
+    })
+  )
+  a.setAttribute('download', fileName)
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+
+  // if (navigator.msSaveBlob) {
+  //   // IE10
+  //   navigator.msSaveBlob(
+  //     new Blob([content], {
+  //       type: mimeType,
+  //     }),
+  //     fileName
+  //   )
+  // } else if (URL && ('download' in a || a.download !== undefined)) {
+  //   // html5 A[download]
+  //   a.href = URL.createObjectURL(
+  //     new Blob([content], {
+  //       type: mimeType,
+  //     })
+  //   )
+  //   a.setAttribute('download', fileName)
+  //   document.body.appendChild(a)
+  //   a.click()
+  //   document.body.removeChild(a)
+  // } else {
+  //   location.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(content)
+  // }
 }
 
 // function generateBins({ min, max }) {
